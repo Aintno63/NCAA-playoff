cb4f7c3287505f17b3f11e9aa4d7926fce83b89a
Aintno63 <sean.middleton63@gmail.com>
2016-06-16 09:44:19 -0500

    create NCAAF_stats_scrape.py

---
+++ NCAAF_stats_scrape.py
@@ -0,0 +1,8 @@
+# Usage: Edit the settings file (scrapersettings.py) and simply run this script
+##############################################################
+
+# No editing is required here
+python create_team_mappings.py
+python create_schedule_mappings.py
+python create_player_mappings_and_agg_stats.py
+python create_ind_stats.py
