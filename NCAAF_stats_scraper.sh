72b6dbd45a3692c24194259d28a2f6544a812f64
Aintno63 <sean.middleton63@gmail.com>
2016-07-13 13:40:17 -0500

    Create NCAAF_stats_scraper.sh

---
+++ NCAAF_stats_scraper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##############################################################
+# Program name: NCAA FOOTBALL Stats Scraper
+# Version: 1.0
+# By: Sean Middleton
+# License: MPL 2.0 (see LICENSE file in root folder)
+# Additional thanks: 
+# Usage: Edit the settings file (scrapersettings.py) and simply run this script
+##############################################################
+
+# No editing is required here
+python create_team_mappings.py
+python create_schedule_mappings.py
+python create_player_mappings_and_agg_stats.py
+python create_ind_stats.py
